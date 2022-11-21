7e7531cea822dcdd188a4dd59e53db0ee67bd60d
David De Santiago <107174944+ddesantiag0@users.noreply.github.com>
2022-11-21 12:39:20 -0800

    Update TestListExamples.java

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,10 +1,52 @@
+import org.junit.Test;
+import java.util.List;
 import static org.junit.Assert.*;
-import org.junit.*;
+import java.util.ArrayList;
 
 public class TestListExamples {
-  @Test(timeout = 100)
-  public void testTimeout() {
-    while(true) {}
-  }
   // Write your grading tests here!
+  @Test
+  public void filterTest(){
+    StringChecker sc = new StringChecker() {
+
+      public boolean checkString(String s){
+          return s.length() < 8;
+      }
+  };
+
+
+  
+  ArrayList<String> list = new ArrayList<>();
+  list.add("Banjo");
+  list.add("Patel");
+  list.add("Jareky");
+
+  List<String> newList = ListExamples.filter(list, sc);
+
+  ArrayList<String> expected = new ArrayList<>();
+  expected.add("Banjo");
+  expected.add("Patel}");
+
+
+  assertArrayEquals(expected.toArray(), newList.toArray());
+  }
+
+  @Test
+  public void mergeTest(){
+    List<String> list1 = new ArrayList<>();
+
+    list1.add("Jareky");
+    list1.add("Banjo}");
+
+    List<String> list2 = new ArrayList<>();
+    list2.add("Patel");
+
+    List<String> newList = ListExamples.merge(list1, list2);
+    List<String> expected = new ArrayList<>();
+    expected.add("Jareky");
+    expected.add("Banjo");
+    expected.add("Patel");
+
+    assertArrayEquals(expected.toArray(), newList.toArray());
+  }
 }
