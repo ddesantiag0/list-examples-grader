9736c5faa6569216381eb69f7ed7ed0e69488caf
David De Santiago <107174944+ddesantiag0@users.noreply.github.com>
2022-11-21 12:40:32 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -1,9 +1,12 @@
 # Create your grading script here
 
+set -e
+CPATH=".:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar"
 rm -rf student-submission
 git clone $1 student-submission
-echo 'Finished cloning'
+cp TestListExamples.java student-submission/
+cp -r lib student-submission
+cd student-submission
 
-
-
-
+javac -cp $CPATH *.java
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples
