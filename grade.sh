9d86c96b9ca5988972e4ed90dcfb1c9eeafd08ad
David De Santiago <107174944+ddesantiag0@users.noreply.github.com>
2022-11-23 15:11:20 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -1,12 +1,32 @@
 # Create your grading script here
 
-set -e
-CPATH=".:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar"
 rm -rf student-submission
 git clone $1 student-submission
-cp TestListExamples.java student-submission/
-cp -r lib student-submission
+
+echo "Cloning..."
+
 cd student-submission
 
-javac -cp $CPATH *.java
-java -cp $CPATH org.junit.runner.JUnitCore TestListExamples
+if [ -f ListExamples.java ]
+then
+    echo "Found student's file."
+else
+    echo "Student file not found."
+    exit
+fi
+
+cp ListExamples.java ..
+
+cd ..
+
+javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java
+
+if [ $? -ne 0 ]
+then
+    echo "Compiler Error"
+    exit
+fi
+
+java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples > grade.txt
+
+grep -w "run:"
